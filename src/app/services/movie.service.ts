1eb6d878c1bd5cba0ec1c0d24c556ba539ec765f
Ricardo Colares <ricardoclr@hotmail.com>
2019-02-07 19:50:13 -0300

    get movie and get movie details configured in services

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export enum SearchType {
+  all = '',
+  movie = 'movie',
+  series = 'series',
+  episode = 'episode',
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
+  url = 'http://www.omdbapi.com/';
+  apiKey = '2be398c0';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  searchData(title: string, type: SearchType): Observable<any> {
+    return this.http
+      .get(
+        `${this.url}?s${encodeURI(title)}$type=${type}&apukey=${this.apiKey}`
+      )
+      .pipe(
+        map(results => {
+          console.log('RAW: ', results);
+          return results['Search'];
+        })
+      );
+  }
+
+  getDetails(id) {
+    this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`);
+  }
 }
